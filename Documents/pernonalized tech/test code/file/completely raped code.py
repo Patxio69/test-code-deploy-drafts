afc2f695386d69b7b9306700c24dc31ac04ace17
AlphCode <38281563+Patxio69@users.noreply.github.com>
2019-09-06 10:12:32 +0530

    just another raped up

---
+++ Documents/pernonalized tech/test code/file/completely raped code.py
@@ -0,0 +1,34 @@
+#this code is only for leaning, it doesn't make any snese what so ever 
+def timeout_trackback():
+    #will be assigned blocked soon
+    dataset = {
+        "data1" : 0
+        #and all the datasets
+        }
+    for loop in dataset:
+        if dataset > 0:
+            return loop
+#decides whetehr to call time_traceback() or other depenging on upload parameter
+def fucc():
+    threashold = 50
+    time = 10
+    for upload_time in range(time):
+        for upload_threashold in threashold:
+         if upload_time > 10:
+             return timeout_trackback()
+         
+         elif upload_threashold <= 10:
+             return function
+
+         else:
+             return "shit! there's" + time*0 + "time" + ":("
+
+for uploading in fucc():
+    if uploading < 1:
+        pass
+    elif uploading > 2:
+        return timeout_trackback()
+
+    
+
+print.mainloop() 
